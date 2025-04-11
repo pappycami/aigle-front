21073aa22a1144e3e7c32e66a61c425fed759d8b
TSIRY HERY Ny Aina Papy <nap@esokia-webagency.com>
2025-04-11 15:09:02 +0300

    fix - depart project

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+// postcss.config.js (ESM compatible)
+import tailwindcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
+
+export default {
+  plugins: [
+    tailwindcss(),
+    autoprefixer(),
+  ],
+}
+
+  

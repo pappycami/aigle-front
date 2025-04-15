f909f693ccb30955b821f8bf2eb80210eb7d8e3c
TSIRY HERY Ny Aina Papy <nap@esokia-webagency.com>
2025-04-15 17:57:17 +0300

    add - utilisation d'alias

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,12 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@types': path.resolve(__dirname, './src/types'),
     },
   },
 })

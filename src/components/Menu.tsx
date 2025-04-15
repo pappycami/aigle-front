583b186c55e706e72b01c0d97a429cba402d671f
TSIRY HERY Ny Aina Papy <nap@esokia-webagency.com>
2025-04-15 17:50:53 +0300

    add - utilisation de @

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../hooks/useAuth";
+import { useAuth } from "@/hooks/useAuth";
 import toast from "react-hot-toast";
 
 export default function Menu() {

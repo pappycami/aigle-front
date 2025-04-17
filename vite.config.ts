6f2004fe82fe33e0309d439b49bdb32a6e3a18a5
TSIRY HERY Ny Aina Papy <nap@esokia-webagency.com>
2025-04-17 16:05:46 +0300

    modif - to albatros

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    allowedHosts: ["two.test", "localhost", "127.0.0.1"],
+    allowedHosts: ["albatros.prx", "localhost", "127.0.0.1"],
   },
   plugins: [react()],
   resolve: {
